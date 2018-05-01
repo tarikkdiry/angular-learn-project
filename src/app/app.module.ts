61fd8dc5fad8ae642f0e43747e74bbf377787d80
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-04-30 22:25:47 -0400

    feature to add bucket list items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule            
   ],
   providers: [],
   bootstrap: [AppComponent]
