9bd5f316a33806f4be7a2c236ea3f26a09c39552
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-05-03 23:46:16 -0400

    update services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,19 +34,23 @@ export class HomeComponent implements OnInit {
   goalText: string = 'My first life goal';
   goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
     this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i) {
     this.goals.splice(i, 1); //Removes item from Goals array
+    this._data.changeGoal(this.goals);
   }
 }
